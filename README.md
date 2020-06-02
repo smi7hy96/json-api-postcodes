3f4449a506560fec651c991e7378a2ddaa8d4bfa
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-06-02 19:26:05 +0100

    added request file, 1 GET example

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # JSON, API AND POSTCODES.IO
+
+## What is an API
+
+## What is JSON
+
+## HTTP GET VS POST
+
+### Main Status Codes
+
+## Package Managers
+
+what is it?
+
+### List the package managers
+
+Find names for 
+- Linux - ubuntu
+- python 
+- ruby
+- js
+- windows
+- IOS
+
+# what is pypi.org
---
+++ get_request_postcodes.py
@@ -0,0 +1,45 @@
+# 1st we need a way to talk HTTP
+# We'll use the requests package that is very stable and widely used
+
+# This is a standard py library - no need to install
+import time
+
+# Requests is not a standard library and must be installed with a package manager
+# package is an external library of code, not native to python (or vanilla python)
+# A package manager installs and manages this package
+
+# Python, Ruby, JavaScript, Java; almost all modern languages have a package manager
+
+import requests
+
+# I want to make a get request to get more info on my postcode
+# I need to check my API Documentation
+# then build the target URK with patha nd args
+# Then I need to use the package requests to make the request
+# I will receive a JSON that I need to parse into a dictionary
+path = 'http://api.postcodes.io/postcodes/'
+args = '<postcode>>'
+
+# build URL
+url_target = path + args
+print(url_target)
+
+# time to request
+
+response = requests.get(url_target)
+
+print(response)
+print(type(response))
+
+# Parsing or getting dictionary out
+print(response.json())
+response_dictionary = response.json()
+print(type(response_dictionary))
+#
+# for key in response_dictionary.keys():
+#     print(key)
+
+result_dictionary = response_dictionary['result']
+
+for key in result_dictionary:
+    print(key, ' IS ', result_dictionary[key])
