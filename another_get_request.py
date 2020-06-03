4530fae8a5f90e6b1efc36b7afbbcfcbaeece57f
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-06-03 12:38:51 +0100

    refined code a little further

--- another_get_request.py
+++ another_get_request.py
@@ -7,15 +7,14 @@ def generate_URL(path, arguments):
 
 def get_response(url):
     response = requests.get(url)
-    check_url = error_handle(response)
-    if check_url:
+    if error_handle(response):
         return long_lat(response)
     else:
         return f'Error occurred when retrieving postcode (error message {response.status_code})'
 
 
 def error_handle(response):
-    if response.status_code == 200:
+    if str(response.status_code).startswith('2') or str(response.status_code).startswith('3'):
         return True
     else:
         return False
